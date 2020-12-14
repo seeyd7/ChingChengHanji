ec80363e4e115773a1aa2fa5f52bf78ce5bc655e
seeyd7 <szymon8866@gmail.com>
2020-12-14 01:48:51 +0100

    siema

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,38 @@
-window.addEventListener("DOMContentLoaded", event => {
-    const audio = document.querySelector("audio");
-    audio.volume = 0.2;
+const audio = new Audio( 'https://dl.dropboxusercontent.com/s/h8pvqqol3ovyle8/tom.mp3' );
+audio.muted = true;
+
+const alert_elem = document.querySelector( '.alert' );
+
+audio.play().then( () => {
+  // already allowed
+  alert_elem.remove();
+  resetAudio();
+} )
+.catch( () => {
+  // need user interaction
+  alert_elem.addEventListener( 'click', ({ target }) => {
+    if( target.matches('button') ) {
+      const allowed = target.value === "1";
+      if( allowed ) {
+        audio.play()
+          .then( resetAudio );
+      }
+      alert_elem.remove();
+    }
+  } );
+} );
+
+document.getElementById( 'btn' ).addEventListener( 'click', (e) => {
+  if( audio.muted ) {
+    console.log( 'silent notification' );
+  }
+  else {
     audio.play();
-  });
-  
+  }
+} );
+
+function resetAudio() {
+  audio.pause();
+  audio.currentTime = 0;
+  audio.muted = false;
+}
